497502b8b859230bb8a772322f800a33c0e78fe9
Jeff Eng <jeff@jeffeng.me>
2016-07-01 15:03:35 -0700

    created a for in loop to loop through an array

--- Practicing Loops in Swift.playground/Contents.swift
+++ Practicing Loops in Swift.playground/Contents.swift
@@ -2,9 +2,15 @@
 
 import UIKit
 
-//Use a for loop to print even numbers from 2 through 20
+//Use a for in loop to print even numbers from 2 through 20
 
 for index in 2.stride(through: 20, by: 2) {
     print(index)
 }
 
+//Practice looping through an array
+var fruits = ["pineapple", "orange", "mango", "kiwi", "strawberry", "blueberry", "cherry"]
+
+for fruit in fruits {
+    print("Eating some \(fruit) everyday keeps the doctor away.")
+}
