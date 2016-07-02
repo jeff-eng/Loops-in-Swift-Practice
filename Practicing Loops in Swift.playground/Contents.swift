5ddd1b29648db472cc5bdcef8c981ff607f71409
Jeff Eng <jeff@jeffeng.me>
2016-07-01 18:50:39 -0700

    wrote while loop for last challenge that subtracts 1 from each integer in array

--- Practicing Loops in Swift.playground/Contents.swift
+++ Practicing Loops in Swift.playground/Contents.swift
@@ -2,7 +2,9 @@
 
 import UIKit
 
-//Challenge: Use a for in loop to print even numbers from 2 through 20
+//FOR LOOPS
+
+//Challenge #1: Use a for in loop to print even numbers from 2 through 20
 
 for index in 2.stride(through: 20, by: 2) {
     print(index)
@@ -27,10 +29,87 @@ for (index, number) in arrayOfIntegers.enumerate() {
 print(arrayOfIntegers)
 
 
-//Challenge: Create an array of integers and halve each value in the array
+//Challenge #2: Create an array of integers and halve each value in the array
 var arrayOfWholeNumbers: [Double] = [4, 8, 9, 89, 16, 23, 46]
 
 for (index, number) in arrayOfWholeNumbers.enumerate() {
     arrayOfWholeNumbers[index] = number / 2
 }
 print(arrayOfWholeNumbers)
+
+
+
+//WHILE LOOPS
+
+//Basic while loop
+
+var i = 1
+
+while i < 10 {
+    print(i)
+    i += 1
+}
+
+//Challenge #3: Use while loop to display the first 10 terms of multiples of 5 (5, 10, 15, 20, etc.)
+
+var x = 1
+
+while x < 11 {
+    print(x * 5)
+    x += 1
+}
+
+//Using a while loop to iterate through an array
+var anotherArray = [2, 5, 5, 7, 2, 9, 34, 3]
+
+var index = 0
+
+while index < anotherArray.count {
+    print(anotherArray[index])
+    index += 1
+}
+
+//Challenge #4: Use a while loop subtract 1 from each item in an array of numbers
+var andAnotherArray = [15, 25, 34, 74, 14, 78, 43]
+
+var idx = 0
+
+while idx < andAnotherArray.count {
+    print(andAnotherArray[idx] - 1)
+    
+    idx += 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
